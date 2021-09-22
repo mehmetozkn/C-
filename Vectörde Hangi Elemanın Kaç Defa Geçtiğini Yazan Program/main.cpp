fb89e9a780f6c1d6112b89d5ddf3d25163486b37
Mehmet Özkan <mehmet61ozkan@outlook.com>
2021-09-22 15:23:18 +0300

    .

---
+++ Vectörde Hangi Elemanın Kaç Defa Geçtiğini Yazan Program/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+
+    vector<int> nums;
+    nums.push_back(4);
+    nums.push_back(1);
+    nums.push_back(2);
+    nums.push_back(1);
+    nums.push_back(2);
+
+    int max = nums[0];
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] > max)
+        {
+            max = nums[i];
+        }
+    }
+
+    vector<int> ref(max+1);
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        ref[nums[i]]++;
+    }
+
+    for (int i = 0; i < ref.size(); i++)
+    {
+        cout <<  i << " Sayisi " << ref[i] << " Defa Geçiyor." << endl;
+    }
+
+    return 0;
+}
